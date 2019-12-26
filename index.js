2a4a679e298be5f94b23c8c70bfd42e59d173d11
Melissa Real <melissa.real@hotmail.com>
2019-12-26 14:16:03 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+        tutorial = tutorial.split(' ');
+        for (var i = 0; i < tutorial.length; i++) {
+            tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+        }
+        return tutorial.join(' ');
+    });
 }
+
